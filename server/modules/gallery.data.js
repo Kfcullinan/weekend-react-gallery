542c487be5dcc4dac21b921b45a231a7c9097c7a
Keith Cullinan <kfcullinan@gmail.com>
2022-08-29 22:41:06 -0500

    added photo paths to gallery items array

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Husqvarna-701-Vitpilen.jpeg', description: 'Swedish Single Cylinder', likes: 0 },
+    { id: 3, path: 'images/BMW-K100-Cafe-Racer-6.jpeg', description: 'German Boxer', likes: 0},
+    { id: 4, path: 'images/triumph-speed-twim=n-custom-kit-1.jpeg', description: 'British Class', likes: 0}
 ];
 
 module.exports = galleryItems;
